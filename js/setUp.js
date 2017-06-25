1afee069c903f4b7ba1c7e8259c4bd5894d26a0e
Amit binu <binua@mcmaster.ca>
2017-06-25 00:45:08 -0400

    Added a new game rule

--- js/setUp.js
+++ js/setUp.js
@@ -52,59 +52,115 @@ var stopWatch = function(){
 }
 var TimeOut = false;
 var numberOfDots  = 0;
-
+var dot;
 var showDots = function(){
 	context.clearRect(0,0,canvas.width,canvas.height);
-	var dot = new randomDot(canvas.width,canvas.height);
+	dot = new randomDot(canvas.width,canvas.height);
 	console.log(dot.color + " " + dot.x +" " +  dot.y + " " + dot.radius);
 	context.fillStyle = dot.color;
 	context.beginPath();
-	context.arc(dot.x,dot.y,dot.radius,0,2*Math.PI);
+	if(dot.radius ===100){
+		context.rect(dot.x,dot.y,60,60);
+	}
+	else{
+		context.arc(dot.x,dot.y,dot.radius,0,2*Math.PI);
+	}
+	
 	context.fill();
 	context.stroke();
 
-	var left = { x : dot.x -dot.radius , y : dot.y };
-
-	var right = { x : dot.x + dot.radius , y : dot.y };
-
-	var top = { x : dot.x , y : dot.y + dot.radius };
-	
-	var bottom = { x : dot.x , y : dot.y - dot.radius };
-
 	var clickedDot;
 
-	if(TimeOut === true){
-		context.clearRect(0,0,canvas.width,canvas.height);
-		console.log(numberOfDots/)
-	}
-	else{
-		if(canvas.addEventListener){
+	if(canvas.addEventListener){
 		canvas.addEventListener("click",function(e){
-			clickedDot = isDotClicked(top,right,bottom,left, e.clientX, e.clientY);
-			if(clickedDot===true){
-				numberOfDots++;
-				console.log(numberOfDots);
-				showDots();
-			};
-			});		
-		}
+			if(TimeOut === true){
+				context.clearRect(0,0,canvas.width,canvas.height);
+			}
+			else{
+				gameRules(e.clientX, e.clientY);
+			}
+		});		
+	}
 	else if(canvas.attachEvent){
 		canvas.addEventListener("click",function(e){
-			clickedDot = isDotClicked(top,right,bottom,left, e.clientX, e.clientY);
-			if(clickedDot===true){
-				numberOfDots++;
-				showDots();
-			};
-
+			if(TimeOut === true){
+				context.clearRect(0,0,canvas.width,canvas.height);
+			}
+			else{
+				gameRules(e.clientX, e.clientY);
+			}
+			
 		});
-		}
 	}
+
 	
 };
 
-var isDotClicked = function(top,right,bottom,left,clickedX,clickedY){
-	is_X_Range_Ok = (clickedX > left.x) && (clickedX < right.x) ;
-	is_Y_Range_Ok = (clickedY > bottom.y) && (clickedY < top.y);
-	is_Range_Ok = is_X_Range_Ok && is_Y_Range_Ok;
-	return is_Range_Ok;
+var gameRules = function(clientX, clientY){
+				clickedDot = isDotClicked(dot.x,dot.y,dot.radius,clientX,clientY);
+				if(clickedDot===true){
+					if(dot.radius===100){ //Reduces points of clicking recatangle
+						numberOfDots--;
+					}
+					else{ //Adds points for clicking the dot.
+						numberOfDots++;
+					}
+						
+					context.clearRect(0,0,canvas.width,canvas.height);
+					dot = new randomDot(canvas.width,canvas.height);
+					context.fillStyle = dot.color;
+					context.beginPath();
+					if(dot.radius ===100){ //Creates rectangle if the returned radius is 100.
+						context.rect(dot.x,dot.y,60,60);
+					}
+					else{ //Creates a circle / dot if the returned radius is not 100.
+						context.arc(dot.x,dot.y,dot.radius,0,2*Math.PI);
+					}
+					context.fill();
+					context.stroke();
+				
+				}
+				else{ 
+					if(dot.radius === 100){//If the rectangle is not clicked, points will be added.
+						numberOfDots++;
+					}
+					else{//If the dot is not clicked, points will be deducted.
+						numberOfDots--;
+					}
+					
+					context.clearRect(0,0,canvas.width,canvas.height);
+					dot = new randomDot(canvas.width,canvas.height);
+					context.fillStyle = dot.color;
+					context.beginPath();
+					if(dot.radius ===100){
+						context.rect(dot.x,dot.y,60,60);
+					}
+					else{
+						context.arc(dot.x,dot.y,dot.radius,0,2*Math.PI);
+					}
+					context.fill();
+					context.stroke();
+				};
+				console.log(numberOfDots);
+			
+}
+
+var isDotClicked = function(xPoint,yPoint,radius,clickedX,clickedY){
+	if(radius===100){
+		var x_range_ok = (xPoint < clickedX) && (clickedX < xPoint+40);
+		var y_range_ok = (yPoint < clickedY) && (clickedY < yPoint+40);
+		var range_ok = x_range_ok && y_range_ok;
+		return range_ok;
+	}
+	else{
+		console.log("clickedX " + clickedX);
+		console.log("clickedY " + clickedY);
+		var x_dist = Math.abs(clickedX - xPoint);
+		var y_dist = Math.abs(clickedY - yPoint);
+
+		var totalDist = Math.sqrt((x_dist)**2 + (y_dist)**2);
+		var clicked = totalDist < radius;
+		return clicked;
+	}
+	
 };
