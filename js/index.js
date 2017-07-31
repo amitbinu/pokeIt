5e9ff059216d06b6a6fed35c4c37aa087ed6714d
Amit binu <binua@mcmaster.ca>
2017-07-31 14:31:09 -0400

    Added game over box with replay button

--- js/index.js
+++ js/index.js
@@ -3,22 +3,26 @@ var time;
 var timeInterval;
 var clickedOnce = false;
 var circle;
-
+var currentuser;
 
 function setup(){
 	$("#menuBar").hide();
 	$("#menuBar").slideDown(1000);
 	firebase.auth().onAuthStateChanged(function (user){
 	 	if(user){
+	 		currentuser = user;
 	 		startButton = select("#StartButton");
 			time= select("#time");
 			circle = select('#circle1');
 			userName = select('#userName');
 			rank = select('#userRank');
-			console.log(user);
-			var name = user.displayName.split();
-
-			userName.html("Welcome " + name[0]);
+			var name = user.displayName.split(" ");
+			name = name[0];
+			var temp = name[0].toUpperCase();
+			for (var i = 1; i < name.length; i++) {
+				temp += name[i];
+			}
+			userName.html("Welcome " + temp);
 			time.hide();
 			if(!clickedOnce){
 				startButton.mousePressed(hideStartButton);
@@ -32,6 +36,8 @@ function setup(){
 	var signoutbutton = select("#signOut");
 	signoutbutton.mousePressed(signout);
 	
+	var signoutbutton2 = select("#signOut2");
+	signoutbutton2.mousePressed(signout);
 	
 }
 
@@ -47,9 +53,9 @@ function hideStartButton(){
 }
 
 var score = 0;
-
+var highScorenum = 90;
 var updateScoreandTime = function(){
-	userName.html("Time Left : 30");
+	userName.html("Time Left : 30 seconds");
 	rank.html("Score : 0");
 		$("#userRank").slideDown(500);
 		$('#userName').slideDown(500);
@@ -58,17 +64,19 @@ var startwatch;
 var stopwatch = function(){
 	startwatch = setInterval(count1,1000);
 }
-var counter1 = 30;
+var counter1 = 5;
 var count1 = function(){
-		console.log(counter1);
-		userName.html("Time Left : " + counter1 + " seconds");
+		userName.html("Time Left : " + --counter1 + " seconds");
 		if (counter1===0) {
 			clearInterval(startwatch);
 			circle.hide();
-			var gameover = select("#GameOver");
-			//gameover.show();
+			gameOver();
 		}
-		counter1--;
+		if (counter1 <= 5 && counter1 != 0) {
+			time.html(counter1);
+			time.show();
+		}
+		
 }
 
 function startcount(){
@@ -100,7 +108,7 @@ function countdown(){
 function explode(){
 	dontBounce=false;
 	
-	if (counter1 > 0) {
+	if (startgame === true) {
 		rank.html("Score : " + ++score);
 		$('#circle1').effect('explode',{'duration':220,'complete': makenewcircle});
 	}
@@ -115,7 +123,7 @@ function makenewcircle(){
 	var newcircle = new randomDot();
 	circle.position(newcircle.x, newcircle.y);
 	circle.style("background", newcircle.color);
-	if(newcircle.radius != 100){
+	if(startgame===true){
 		circle.style("width", newcircle.radius+'em');
 		circle.style("height",newcircle.radius+'em');		
 		circle.show();
@@ -125,10 +133,50 @@ function makenewcircle(){
 	
 	}
 	else{
-		makenewcircle(); //make 'circle' look like rectangle and if its clicked , it should shake. 
+		circle.hide();
 	}
 	
 	
+}
+
+function gameOver(){
+
+	firebase.database().ref("users/"+currentuser.uid+"/highScore").once('value').then(function(snapshot){
+		highScorenum = snapshot.val();
+		console.log(highScorenum);
+		if(highScorenum === undefined){
+		firebase.database().ref("users/"+currentuser.uid).set({
+					name: currentuser.displayName,
+					highScore: score
+				});
+		highScorenum = score;
+	}
+	else{
+		if (score >= highScorenum) {
+			firebase.database().ref("users"+currentuser.uid).set({
+				highScore: score
+			});
+			highScorenum = score;
+		}
+	}
+	var currentScore = select("#currentScore");
+	currentScore.html("Score : " + score);
+	var highScore = select("#highScore");
+	highScore.html("High Score : " + highScorenum);
+	var gameover = select("#GameOver");
+	var currentRank = select("#currentRank");
+	currentRank.html("Current Rank : 5");
+	gameover.show();
+	});
+	
+	startgame = false;
+	time.hide();
+	
+	circle.hide();
+	var replay = select("#replay");
+	replay.mousePressed(function(){
+		window.location.href = "game.html";
+	});
 }
 
 function windowResized(){
@@ -143,5 +191,11 @@ function signout(){
 		window.location.href = "index.html";
 	}, function(error){
 		console.log('Sign Out error', error);
+	});
+}
+
+function getstuff(){
+	return firebase.database().ref("users/"+currentuser.uid+"/highScore").once('value').then(function(snapshot){
+		var highscore = snapshot.val();
 	});
 }
