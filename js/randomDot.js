f11a6b35059e4ae8823a09a94801330a2a2b47cc
Amit binu <binua@mcmaster.ca>
2017-08-03 17:18:09 -0400

    Fixed the bug of half circles appearing

--- js/randomDot.js
+++ js/randomDot.js
@@ -1,15 +1,30 @@
 var randomDot = function(){
 	minHeight = windowHeight *0.2;
-	maxHeight = windowHeight*0.85;
+	maxHeight = windowHeight*0.80;
 
-	minWidth = windowWidth * 0.05;
+	minWidth = windowWidth * 0.03;
 	maxWidth = windowWidth * 0.85;
 
-	this.x = getRandomIntInclusive(minWidth,maxWidth);
-	this.y = getRandomIntInclusive(minHeight,maxHeight);
+	
 	
 	this.color = randomColor();
-	this.radius = randomRadius();
+	if(windowWidth > 800){
+		this.radius =8; //randomRadius();
+	}
+	if(windowWidth > 600 && windowWidth <= 800){
+		maxHeight = windowHeight*0.85;
+		this.radius = 6; //randomRadius2();
+	}
+	if(windowWidth > 400 && windowWidth <= 600){
+		maxHeight = windowHeight*0.88;
+		this.radius = 5; //randomRadius3();
+	}
+	if(windowWidth <= 400){
+		maxHeight = windowHeight*0.90;
+		this.radius = 3; //randomRadius4();
+	}
+	this.x = minWidth;//getRandomIntInclusive(minWidth,maxWidth);
+	this.y = maxHeight; //getRandomIntInclusive(minHeight,maxHeight);
 }
 
 function getRandomIntInclusive(min, max) {
@@ -38,6 +53,7 @@ var randomColor = function(){
 
 var randomRadius = function(){
 	var randomNumber = getRandomIntInclusive(0,7);
+	return 9;
 	switch(randomNumber){
 		case 0:
 			return 5;
@@ -55,5 +71,70 @@ var randomRadius = function(){
 			return 8;
 		case 7:
 			return 9;
+	}
+}
+
+var randomRadius2 = function(){
+	var randomNumber = getRandomIntInclusive(0,7);
+	return 6;
+	switch(randomNumber){
+		case 0:
+			return 1;
+		case 1:
+			return 2;
+		case 2:
+			return 3;
+		case 3:
+			return 4;
+		case 4:
+			return 5;
+		case 5:
+			return 6;
+		case 6:
+			return 3.5;
+		case 7:
+			return 2.5;
+	}
+}
+
+var randomRadius3= function(){
+	var randomNumber = getRandomIntInclusive(0,7);
+	return 5;
+	switch(randomNumber){
+		case 0:
+			return 1;
+		case 1:
+			return 2;
+		case 2:
+			return 3;
+		case 3:
+			return 4;
+		case 4:
+			return 5;
+		case 5:
+			return 1.5;
+		case 6:
+			return 2.5;
+		case 7:
+			return 3.5;
+	}
+}
+
+var randomRadius4= function(){
+	var randomNumber = getRandomIntInclusive(0,5);
+	return 3;
+	switch(randomNumber){
+		case 0:
+			return 1;
+		case 1:
+			return 2;
+		case 2:
+			return 3;
+		case 3:
+			return 1.5;
+		case 4:
+			return 2.5;
+		case 5:
+			return 3.5;
 	}
 }
