867fb3f5c883d9d05af6f46d787a1023bb6da123
Amit binu <binua@mcmaster.ca>
2017-08-03 17:19:50 -0400

    Added animation to the video

--- js/signIn.js
+++ js/signIn.js
@@ -1,5 +1,7 @@
 
   // Initialize Firebase
+ 
+
   var config = {
     apiKey: "AIzaSyBx5nUasv7V8ylxUARAHmcD5-4fXPPesqY",
     authDomain: "pokeit-5493a.firebaseapp.com",
@@ -16,13 +18,19 @@ var uiConfig = {
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
           firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
         ],
         // Terms of service url.
         tosUrl: 'https://www.google.com'
       };
 
+
       // Initialize the FirebaseUI Widget using Firebase.
       var ui = new firebaseui.auth.AuthUI(firebase.auth());
       // The start method will wait until the DOM is loaded.
       ui.start('#firebaseui-auth-container', uiConfig);
+
+$(document).ready(function(){
+        $("#gif").hide();
+        $("#gif").slideDown(2000);
+      })
